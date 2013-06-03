706685b72f2f4ca3fbc9b3205d216f02388653e5
Ben Hilburn <ben.hilburn@ettus.com>
2013-06-03 12:37:14 -0700

    Making help list available prior to configuration.

--- pybombs.py
+++ pybombs.py
@@ -1,7 +1,55 @@
 #!/usr/bin/env python
 
-# remove this for deployment
 import os,re,sys;
+
+"""
+Handle commands that don't require loading the PyBombs modules & subsystem.
+"""
+if(sys.argv[1] == "help" or sys.argv[1] == "--help"):
+    print "Python Build Overlay Managed Bundle Systems - CLI"
+    print "Usage: %s <op> <args...>"%(sys.argv[0])
+    print ""
+    print " Operations:"
+    print "     help            - show this screen"
+    print "     list            - show installed & available packages"
+    print "     info <pkg>      - display package info"
+    print "     install <pkg>   - install a package"
+    print "     remove          - cleanly remove all fully installed packages and dependents"
+    print "     remove <pkg>    - cleanly remove a fully installed package and dependents"
+    print "     rnd <pkg>       - cleanly remove a fully installed package (*DANGER* can break dependees!)"
+    print "     clean <pkg>     - ensure a package is in the uninstalled state"
+    print "     config          - show all local config settings"
+    print "     config <k>      - show one local config setting"
+    print "     config <k> <v>  - set a local config setting"
+    print "     inv             - show inventory values"
+    print "     inv <k>         - clear inventory value for k"
+    print "     inv <k> <v>     - update local package k inventory value to v"
+    print "     digraph         - write out package.dot digraph for graphviz (all pkgs)"
+    print "     digraphi        - write out package.dot digraph for graphviz (installed pkgs)"
+    print "     env             - write out env"
+    print "     update          - update all packages"
+    print "     update <pkg>    - update a specific package"
+    print "     rb <pkg>        - rebuild without re-fetch (equivalent to [inv pkg fetch && install pkg])"
+    print "     search <expr>   - search for a package recipe matching <expr>"
+    print ""
+    exit(0)
+
+if(sys.argv[1] == "moo"):
+    print "         (__)    "
+    print "         (oo)    "
+    print "   /------\/     "
+    print "  / |    ||      "
+    print " *  /\---/\      "
+    print "    ~~   ~~      "
+    print "....\"Have you mooed today?\"..."
+    exit(0)
+
+
+"""
+Load the PyBomb modules & subsystem. Prepare for awesome.
+"""
+
+# remove this for deployment
 files = os.listdir("mod_pybombs");
 for f in files:
     if(re.match("\w+.pyc", f)):
@@ -105,43 +153,6 @@ if(sys.argv[1] == "inv"):
         inventory_set(sys.argv[2], sys.argv[3])
     else:
         sys.argv[1] = "help"
-
-if(sys.argv[1] == "moo"):
-    print "         (__)    "
-    print "         (oo)    "
-    print "   /------\/     "
-    print "  / |    ||      "
-    print " *  /\---/\      "
-    print "    ~~   ~~      "
-    print "....\"Have you mooed today?\"..."
-
-if(sys.argv[1] == "help" or sys.argv[1] == "--help"):
-    print "Python Build Overlay Managed Bundle Systems - CLI"
-    print "Usage: %s <op> <args...>"%(sys.argv[0])
-    print ""
-    print " Operations:"
-    print "     help            - show this screen"
-    print "     list            - show installed & available packages"
-    print "     info <pkg>      - display package info"
-    print "     install <pkg>   - install a package"
-    print "     remove          - cleanly remove all fully installed packages and dependents"
-    print "     remove <pkg>    - cleanly remove a fully installed package and dependents"
-    print "     rnd <pkg>       - cleanly remove a fully installed package (*DANGER* can break dependees!)"
-    print "     clean <pkg>     - ensure a package is in the uninstalled state"
-    print "     config          - show all local config settings"
-    print "     config <k>      - show one local config setting"
-    print "     config <k> <v>  - set a local config setting"
-    print "     inv             - show inventory values"
-    print "     inv <k>         - clear inventory value for k"
-    print "     inv <k> <v>     - update local package k inventory value to v"
-    print "     digraph         - write out package.dot digraph for graphviz (all pkgs)"
-    print "     digraphi        - write out package.dot digraph for graphviz (installed pkgs)"
-    print "     env             - write out env"
-    print "     update          - update all packages"
-    print "     update <pkg>    - update a specific package"
-    print "     rb <pkg>        - rebuild without re-fetch (equivalent to [inv pkg fetch && install pkg])"
-    print "     search <expr>   - search for a package recipe matching <expr>"
-    print ""
 
 #if(sys.argv
 #op = sys.argv[1];
