a57705585c1d962ebf251211213292d126b92838
Tom Rondeau <trondeau@vt.edu>
2013-06-19 11:09:36 -0400

    Removed .py extension off pybombs.

--- jenkins.sh
+++ jenkins.sh
@@ -8,9 +8,9 @@ echo "gituser = hudson" >> config.dat
 echo "prefix = /target" >> config.dat
 echo "satisfy_order = $satisfy_order"  >> config.dat
 echo "timeout = 30" >> config.dat
-./pybombs.py inv gr-baz installed           # CMake Error at CMakeLists.txt:135 (message): Gruel required to compile baz
-./pybombs.py inv gr-osmosdr installed      # CMake Error at CMakeLists.txt:123 (message): Gruel required to build gr-osmosdr
-./pybombs.py inv gnss-sdr installed         # CMake Error at CMakeLists.txt:125 (message): Please install GNU Radio and all its dependencies.
-./pybombs.py inv kal installed              # bash: line 2: ./configure: No such file or directory
-./pybombs.py install all
-./pybombs.py env
+./pybombs inv gr-baz installed           # CMake Error at CMakeLists.txt:135 (message): Gruel required to compile baz
+./pybombs inv gr-osmosdr installed      # CMake Error at CMakeLists.txt:123 (message): Gruel required to build gr-osmosdr
+./pybombs inv gnss-sdr installed         # CMake Error at CMakeLists.txt:125 (message): Please install GNU Radio and all its dependencies.
+./pybombs inv kal installed              # bash: line 2: ./configure: No such file or directory
+./pybombs install all
+./pybombs env
