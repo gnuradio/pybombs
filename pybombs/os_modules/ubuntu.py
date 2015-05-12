483f3ea5404c9ba4fc51737280ee99ed44382522
Seth Hitefield <sdhitefield@gmail.com>
2015-05-12 13:00:52 -0400

    Added initial version of the system_manager.

--- pybombs/os_modules/ubuntu.py
+++ pybombs/os_modules/ubuntu.py
@@ -22,10 +22,136 @@
 OS Module: Ubuntu
 """
 
-class OSModuleUbuntu(OSModuleBase):
+import re
+import subprocess
+
+
+class Ubuntu(object):
     def __init__(self):
         pass
 
     def satisfy(self, pkgname, version):
         pass
 
+    def supported():
+        """ Return true if this platform is detected """
+        # Check to make sure that dpkg, apt-cache and apt-get are installed
+        '''
+        def have_debs(pkg_expr_tree):
+            if(which("dpkg") == None):
+                return False;
+            if(pkg_expr_tree):
+                return pkg_expr_tree.ev(have_deb);
+            return False;
+
+        def debs_exist(pkg_expr_tree):
+            if(which("apt-cache") == None):
+                return False
+            if(pkg_expr_tree):
+                return pkg_expr_tree.ev(deb_exists);
+            return False
+        '''
+
+    def exists(self, name, throw_ex=True):
+        """
+        Checks to see if a package is avaliable in apt-get and returns the verion.
+        If package does not exist, throw an exception.
+        """
+
+        try:
+            out = subprocess.check_output(["apt-cache", "showpkg", name])
+            # apt-cache returns nothing on stdout if a package is not found
+            if len(out) >= 0:
+                # Get the versions
+                ver = re.search(r'Versions: \n(\d+:)?([0-9]+\.[0-9]+\.[0-9]+|[0-9]+\.[0-9]+|[0-9]+[a-z]+|[0-9]+).*\n', out)
+                return ver.group(2)
+            else:
+                msg = "Unknown package"
+        except subprocess.CalledProcessError, e:
+            # Non-zero return.
+            msg = "Call to showpkg failed"
+        except:
+            # The subprocess call failed
+            msg = "Subprocess call failed"
+
+        # Determine the correct output method
+        if throw_ex:
+            raise Exception(msg)
+        return False
+
+    def install(self, name, throw_ex=True):
+        try:
+            out = subprocess.check_output(["sudo", "apt-get", "-y", "install", name])
+            return True
+        except subprocess.CalledProcessError, e:
+            # Non-zero return.
+            msg = "Call to showpkg failed"
+        except:
+            # The subprocess call failed
+            msg = "Subprocess call failed"
+
+        # Determine the correct output method
+        if throw_ex:
+            raise Exception(msg)
+        return False
+
+    def installed(self, name, throw_ex=True):
+        try:
+            out = subprocess.check_output(["dpkg", "-s", name])
+
+            # Get the versions
+            ver = re.search(r'Version: (\d+:)?([0-9]+\.[0-9]+\.[0-9]+|[0-9]+\.[0-9]+|[0-9]+[a-z]+|[0-9]+).*\n', out)
+            return ver.group(2)
+        except subprocess.CalledProcessError, e:
+            # Non-zero return.
+            msg = "Call to showpkg failed"
+        except:
+            # The subprocess call failed
+            msg = "Subprocess call failed"
+
+        # Determine the correct output method
+        if throw_ex:
+            raise Exception(msg)
+        return False
+
+    def execute(self, name):
+        """
+        Checks to see if a package is avaliable in apt-get and returns the verion.
+        If package does not exist, throw an exception.
+        """
+
+        try:
+            if not isinstance(cmd, list):
+                cmd = [cmd]
+            out = subprocess.check_output(cmd)
+            return out
+        except subprocess.CalledProcessError, e:
+            return False
+        except:
+            return False
+
+    # stdout -> return
+    def execute_err(cmd, throw=True):
+        try:
+            if not isinstance(cmd, list):
+                cmd = [cmd]
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=pb_globals.env);
+            (out,err) = p.communicate();
+            return out;
+        except Exception, e:
+            if(throw_ex):
+                raise e;
+            else:
+                return -1;
+
+ubuntu = Ubuntu()
+
+# Some test code:
+if __name__ == "__main__":
+    print ubuntu.exists("gcc")
+    print ubuntu.installed("gcc")
+    print ubuntu.install("gcc")
+    print ubuntu.exists("test", throw_ex=False)
+    print ubuntu.installed("test", throw_ex=False)
+    print ubuntu.install("test", throw_ex=False)
+    print ubuntu.exists("test")
