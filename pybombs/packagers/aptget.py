d28d6cd30f6454e3b4a67d7f363989e2b5c20c48
Martin Braun <martin.braun@ettus.com>
2015-08-08 12:21:53 -0700

    More compatible version reading from dpkg

--- pybombs/packagers/aptget.py
+++ pybombs/packagers/aptget.py
@@ -115,7 +115,11 @@ class AptGet(PackagerBase):
             out = subprocess.check_output(["dpkg", "-s", pkgname], stderr=subprocess.STDOUT)
             # Get the versions
             #ver = re.search(r'^Version: (?:\d+:)?([0-9]+\.[0-9]+\.[0-9]+|[0-9]+\.[0-9]+|[0-9]+[a-z]+|[0-9]+).*\n', out)
-            ver = re.search(r'^Version: (?:\d+:)?(?P<ver>[0-9.]*)$', out, re.MULTILINE).group('ver')
+            ver = re.search(r'^Version: (?:\d+:)?(?P<ver>[0-9]+\.[0-9]+\.[0-9]+|[0-9]+\.[0-9]+|[0-9]+[a-z]+|[0-9]+)', out, re.MULTILINE)
+            if ver is None:
+                self.log.debug("Looks like dpkg -s can't find package {pkg}".format(pkg=pkgname))
+                return False
+            ver = ver.group('ver')
             self.log.debug("Package {} has version {} in dpkg".format(pkgname, ver))
             return ver
         except subprocess.CalledProcessError:
