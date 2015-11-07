148f9ccd244650db3a3905eb2f5d1ba1c441a1a2
Nathan West <nathan.west@okstate.edu>
2015-11-06 20:57:49 -0500

    implement get_console_width()

--- mod_pybombs/output_proc.py
+++ mod_pybombs/output_proc.py
@@ -33,9 +33,32 @@ READ_TIMEOUT = 0.1 # s
 
 ROTATION_ANIM = ('-', '\\', '|', '/',)
 
-# FIXME write this
 def get_console_width():
-    return 80
+    '''
+    http://stackoverflow.com/questions/566746/how-to-get-console-window-width-in-python
+    '''
+    import os
+    env = os.environ
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl, termios, struct, os
+            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
+        '1234'))
+        except:
+            return
+        return cr
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+    if not cr:
+        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+
+    return cr[1]
 
 class OutputProcessor(object):
     """ Output processor. Meant to parse output from a running command. """
