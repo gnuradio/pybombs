dd2aed22df67099793e7724d33254457dd327c81
Martin Braun <martin.braun@ettus.com>
2015-12-11 17:16:09 -0800

    packagers: Add a check-command packager
    Note: Still needs activation in packagers/__init__.py and
    uncommenting of lines in .lwr files, pending merges of the
    requirements parser.

--- pybombs/packagers/cmd.py
+++ pybombs/packagers/cmd.py
@@ -19,31 +19,31 @@
 # Boston, MA 02110-1301, USA.
 #
 """
-Packager: pkg-config
+Pseudo-Packager: Test command
 """
 
 import re
 import subprocess
 from pybombs.packagers.base import PackagerBase
-from pybombs.utils import sysutils
 from pybombs.utils.vcompare import vcompare
 
-class PkgConfig(PackagerBase):
+class TestCommand(PackagerBase):
     """
-    Uses pkg-config. Can't really install stuff, but is useful for
+    Try querying a tool by runnning a command.
+    Can't really install stuff, but is useful for
     finding out if something is already installed.
     """
-    name = 'pkgconfig'
-    pkgtype = 'pkgconfig'
+    name = 'cmd'
+    pkgtype = 'cmd'
 
     def __init__(self):
         PackagerBase.__init__(self)
 
     def supported(self):
         """
-        Check if we can even run 'pkg-config'. Return True if yes.
+        Always works
         """
-        return sysutils.which('pkg-config') is not None
+        return True
 
     def _package_exists(self, pkgname, comparator=">=", required_version=None):
         """
@@ -53,40 +53,55 @@ class PkgConfig(PackagerBase):
 
     def _package_installed(self, pkgname, comparator=">=", required_version=None):
         """
-        See if the installed version of pkgname matches the version requirements.
+        See if the installed version of pkgname matches version requirements.
         """
-        installed_version = self.get_version_from_pkgconfig(pkgname)
+        installed_version = self.get_version_from_command(pkgname)
         if not installed_version:
             return False
         if required_version is None or vcompare(comparator, installed_version, required_version):
+            self.log.debug("Package {pkg} found via command line.".format(pkg=pkgname))
             return True
-            self.log.debug("Package {pkg} found by pkg-config.".format(pkg=pkgname))
         return False
 
     def _package_install(self, pkgname, comparator=">=", required_version=None):
         """
-        pkg-config can't install stuff, so this must always fail
+        Can't install stuff, so this must always fail
         """
         return False
 
     def _package_update(self, pkgname, comparator=">=", required_version=None):
         """
-        pkg-config can't update stuff, so this must always fail
+        Can't update stuff, so this must always fail
         """
         return False
 
     ### pkg-config specific functions:
-    def get_version_from_pkgconfig(self, pkgname):
+    def get_version_from_command(self, command):
         """
-        Check which version is currently installed.
+        Run command, see if it works. If the output has a version number in
+        x.y.z format, return that. If it doesn't, but the command ran, return
+        True. If it fails, return False. ezpz.
         """
         try:
-            # pkg-config will return non-zero if package does not exist, thus will throw
-            ver = subprocess.check_output(["pkg-config", "--modversion", pkgname], stderr=subprocess.STDOUT).strip()
-            self.log.debug("Package {0} has version {1} in pkg-config".format(pkgname, ver))
+            # If this fails, it almost always throws.
+            # NOTE: the split is to handle multi-argument commands. There's
+            # cases where this is not intended, e.g. it won't handle argument
+            # with spaces! But currently this is preferable to running the
+            # command in a shell, which would allow arbitrary commands.
+            output = subprocess.check_output(command.split(), stderr=subprocess.STDOUT).strip()
+            ver = re.search(
+                r'^Version: (?:\d+:)?(?P<ver>[0-9]+\.[0-9]+\.[0-9]+|[0-9]+\.[0-9]+|[0-9]+[a-z]+|[0-9]+)',
+                output,
+                re.MULTILINE
+            )
+            if ver is None:
+                self.log.debug("Could run, but couldn't find a version number.")
+                return True
+            ver = ver.group('ver')
+            self.log.debug("Found version number: {0}".format(ver))
             return ver
-        except subprocess.CalledProcessError:
-            # This usually means the packet is not installed
+        except (subprocess.CalledProcessError, OSError):
+            # We'll assume it's not installed
             return False
         except Exception as e:
             self.log.error("Running `pkg-config --modversion` failed.")
