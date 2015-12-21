2d17b24891cfa295f442b7c59b1f01d52d34bff3
Martin Braun <martin.braun@ettus.com>
2015-12-20 22:32:45 -0800

    git: Fixed output processor bug

--- pybombs/fetchers/git.py
+++ pybombs/fetchers/git.py
@@ -26,6 +26,7 @@ import os
 from pybombs.fetchers.base import FetcherBase
 from pybombs.utils import output_proc
 from pybombs.utils import subproc
+from pybombs import pb_logging
 
 class Git(FetcherBase):
     """
@@ -55,7 +56,7 @@ class Git(FetcherBase):
             git_cmd.append(args.get('gitbranch'))
         o_proc = None
         if self.log.getEffectiveLevel() >= pb_logging.DEBUG:
-            o_proc = output_proc.OutputProcessorMake(preamble="Cloning: "),
+            o_proc = output_proc.OutputProcessorMake(preamble="Cloning: ")
         subproc.monitor_process(
             args=git_cmd,
             o_proc=o_proc,
@@ -108,7 +109,7 @@ class Git(FetcherBase):
             ]
         o_proc = None
         if self.log.getEffectiveLevel() >= pb_logging.DEBUG:
-            o_proc = output_proc.OutputProcessorMake(preamble="Updating: "),
+            o_proc = output_proc.OutputProcessorMake(preamble="Updating: ")
         for cmd in git_cmds:
             subproc.monitor_process(
                 args=cmd,
