81914470319d3f71a23e8ab7edc76703f0126947
Martin Braun <martin.braun@ettus.com>
2016-02-22 09:12:01 -0800

    Added GRC_BLOCKS_PATH to setup_env.sh

--- pybombs/skel/setup_env.sh
+++ pybombs/skel/setup_env.sh
@@ -3,6 +3,7 @@ export PATH="{prefix_dir}/bin:$PATH"
 export PYTHONPATH="{prefix_dir}/python:{prefix_dir}/lib/python2.6/site-packages:{prefix_dir}/lib64/python2.6/site-packages:{prefix_dir}/lib/python2.6/dist-packages:{prefix_dir}/lib64/python2.6/dist-packages:{prefix_dir}/lib/python2.7/site-packages:{prefix_dir}/lib64/python2.7/site-packages:{prefix_dir}/lib/python2.7/dist-packages:{prefix_dir}/lib64/python2.7/dist-packages:{prefix_dir}/python/:{prefix_dir}/lib/python2.6/site-packages:{prefix_dir}/lib64/python2.6/site-packages:{prefix_dir}/lib/python2.6/dist-packages:{prefix_dir}/lib64/python2.6/dist-packages:{prefix_dir}/lib/python2.7/site-packages:{prefix_dir}/lib64/python2.7/site-packages:{prefix_dir}/lib/python2.7/dist-packages:{prefix_dir}/lib64/python2.7/dist-packages:$PYTHONPATH"
 export LD_LIBRARY_PATH="{prefix_dir}/lib:{prefix_dir}/lib64/:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="{prefix_dir}/lib/pkgconfig:{prefix_dir}/lib64/pkgconfig:$PKG_CONFIG_PATH"
+export GRC_BLOCKS_PATH="{prefix_dir}/share/gnuradio/grc/blocks:$GRC_BLOCKS_PATH"
 export PYBOMBS_PREFIX="{prefix_dir}"
 # If we're in a Python virtualenv, activate that
 if [ -r {prefix_dir}/bin/activate ]; then
