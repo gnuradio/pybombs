8b4e77969520119bcff607ee63808bd58c0d69c8
Martin Braun <martin.braun@ettus.com>
2016-03-05 16:48:13 -0800

    config: Allow to set package and category flags from the cmd line (Issue #264)

--- pybombs/commands/config.py
+++ pybombs/commands/config.py
@@ -23,6 +23,7 @@
 from __future__ import print_function
 from pybombs.commands import CommandBase
 from pybombs.config_manager import extract_cfg_items
+from pybombs.config_file import PBConfigFile
 
 class Config(CommandBase):
     """ Remove a package from this prefix """
@@ -51,6 +52,14 @@ class Config(CommandBase):
             '-o', '--config-only', action='store_true',
             help="If specified, only consider options from this one file. Default: Consider all config options",
         )
+        parser.add_argument(
+            '-P', '--package',
+            help="If specified, set options for this package instead of the global configs",
+        )
+        parser.add_argument(
+            '-C', '--category',
+            help="If specified, set options for this category instead of the global configs",
+        )
 
     def __init__(self, cmd, args):
         CommandBase.__init__(self,
@@ -65,10 +74,18 @@ class Config(CommandBase):
             self.cfg_file = self.args.config_file
         elif prefix.prefix_dir is not None and prefix.prefix_src == "cli":
             self.cfg_file = prefix.cfg_file
-        self.log.debug("Using config file: {0}".format(self.cfg_file))
+        self.log.info("Using config file: {0}".format(self.cfg_file))
 
     def run(self):
-        """ Go, go, go! """
+        " Go, go, go! "
+        if self.args.package is not None or self.args.category is not None:
+            return self._run_pkg_or_cat()
+        return self._run_config()
+
+    def _run_config(self):
+        """
+        Handle `pybombs config'
+        """
         if self.args.config_only:
             cfg_data = extract_cfg_items(self.cfg_file, 'config', False)
         else:
@@ -83,7 +100,34 @@ class Config(CommandBase):
                 cfg_file=self.cfg_file,
             )
         for key in keys:
-            print("{key}: {value}\n  - {help}".format(
-                key=key, value=cfg_data.get(key, ""), help=self.cfg.get_help(key),
-            ))
+            print("{key}: {value}".format(key=key, value=cfg_data.get(key, "")))
+            print("  - {help}".format(help=self.cfg.get_help(key) or " Undocumented config option"))
+
+    def _run_pkg_or_cat(self):
+        """
+        Handle `pybombs config --package or --category'
+        """
+        if self.args.package is not None and self.args.category is not None:
+            self.log.error("Cannot provide both --package and --category!")
+            return -1
+        if self.args.package is not None:
+            config_target = 'packages'
+            pkg_cat = self.args.package
+        elif self.args.category is not None:
+            config_target = 'categories'
+            pkg_cat = self.args.category
+        cfg_file = PBConfigFile(self.cfg_file)
+        keys = [self.args.key]
+        verb = "Getting"
+        if self.args.key is None:
+            keys = cfg_file.data.get(config_target, {}).get(pkg_cat, {}).keys()
+        elif self.args.value is not None:
+            cfg_file.update({config_target: {pkg_cat: {self.args.key: self.args.value}}})
+            verb = "Setting"
+        print("{verb} keys for {target} {pkgcat}:".format(verb=verb, target=config_target, pkgcat=pkg_cat))
+        for key in keys:
+            print("{key}: {value}".format(
+                key=key,
+                value=cfg_file.data.get(config_target, {}).get(pkg_cat, {}).get(key))
+            )
 
